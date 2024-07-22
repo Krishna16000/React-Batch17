5a94dae36ec23b7490bcd6253d714e1e901c7838
Krishna <panatikrishnakanth@gmail.com>
2024-07-22 21:17:14 +0530

    .

--- myfirstreactapp/src/components/Useeffect.js
+++ myfirstreactapp/src/components/Useeffect.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 function App (){
     const [count, setCount] = useState(0);
-    useEffect(()=> console.log(count),[count])
+    useEffect(()=> console.log(count),[])
     return(
         <div>
             <p>You clicked{count} times</p>
