14f9fde022574a277e96f2119e8ca09435a76ab8
Krishna <panatikrishnakanth@gmail.com>
2024-12-10 12:09:18 +0530

    filter and map

--- myfirstreactapp/src/components/Filter.js
+++ myfirstreactapp/src/components/Filter.js
@@ -1,19 +1,13 @@
 import React from "react";
-
 function App(){
-    const names= ['James','Paul','Rayn', 'George'];
-    const arr=[10,20,30,40,50,60,70,80]
-    const filtered = arr.filter(item=>item<50);
-    return (
+    const arr= [10,20,30,40,50,60,70,80];
+    const filtered = arr.filter((items) =>items>50)
+    return(
         <div>
-            <center>
             {
-                filtered.map(item => <li>{item}</li>)
+                filtered.map(items=><li>{items}</li>)
             }
-            </center>
         </div>
-        
-    )
+    );
 }
-
 export default App;
