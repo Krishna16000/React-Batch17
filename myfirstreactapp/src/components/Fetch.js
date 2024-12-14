16a8347c21b56ad66ff6f66c96811a3690b0f6d0
Krishna <panatikrishnakanth@gmail.com>
2024-12-14 18:51:20 +0530

    fetch

--- myfirstreactapp/src/components/Fetch.js
+++ myfirstreactapp/src/components/Fetch.js
@@ -1,21 +1,21 @@
 import React, {useEffect,useState} from "react";
 
-const App= ()=>{
-    const [data, setData] = useState([]);
+const App=()=>{
+    const [data, setData] = useState([])
     useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/todos').then(
-            response=> response.json()).then(
-                json => setData(json)
+        fetch('https://jsonplaceholder.typicode.com/posts').then(
+            response=>response.json()).then(
+                json=>setData(json)
             )
+        
     },[])
-return(
-    <div>
-        {
-            data.map(items=> <li key={items.id}>{items.title}</li>)
-        }
-    </div>
-)
+    return(
+        <div>
+            {
+                data.map(items=> <li key={items.id}>{items.title}</li>)
+            }
+        </div>
+    )
 }
 
 export default App;
-
